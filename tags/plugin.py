a59fe0236023845cf77c762cf976c991db4517f5
Tai <tai@6sense.com>
2021-05-27 20:24:34 -0700

    fallback to markdown document header if there is no "title" in metadata

--- tags/plugin.py
+++ tags/plugin.py
@@ -4,15 +4,14 @@
 # JL Diaz (c) 2019
 # MIT License
 # --------------------------------------------
-from collections  import defaultdict
+from collections import defaultdict
 from pathlib import Path
-import os
 import yaml
 import jinja2
 from mkdocs.structure.files import File
-from mkdocs.structure.nav import Section
 from mkdocs.plugins import BasePlugin
 from mkdocs.config.config_options import Type
+from mkdocs.utils import meta as meta_util, get_markdown_title
 
 
 class TagsPlugin(BasePlugin):
@@ -106,27 +105,18 @@ class TagsPlugin(BasePlugin):
         with open(str(self.tags_folder / self.tags_filename), "w") as f:
             f.write(t)
 
+
 # Helper functions
 
-def get_metadata(name, path):
-    # Extract metadata from the yaml at the beginning of the file
-    def extract_yaml(f):
-        result = []
-        c = 0
-        for line in f:
-            if line.strip() == "---":
-                c +=1
-                continue
-            if c==2:
-                break
-            if c==1:
-                result.append(line)
-        return "".join(result)
 
+def get_metadata(name, path):
     filename = Path(path) / Path(name)
-    with filename.open() as f:
-        metadata = extract_yaml(f)
-        if metadata:
-            meta = yaml.load(metadata, Loader=yaml.FullLoader)
-            meta.update(filename=name)
+    with open(filename, 'r', encoding='utf-8-sig', errors='strict') as f:
+        source = f.read()
+        md_content, meta = meta_util.get_data(source)
+
+        if meta:
+            if 'title' not in meta:
+                meta['title'] = get_markdown_title(md_content)
+            meta['filename'] = name
             return meta
